317f29673bb8985940105fe76bb4318f327a6d41
Aleksey9374 <noyabrskhome75028@gmail.com>
2024-10-16 10:45:53 +0500

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,11 @@
+import fake_math as f_math
+import true_math as t_math
+
+result1 = f_math.divide(69, 3)
+result2 = f_math.divide(3, 0)
+result3 = t_math.divide(49, 7)
+result4 = t_math.divide(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
